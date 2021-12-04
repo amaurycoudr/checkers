e85a80581b9b4975c634c99133c50b351b538784
Amaury Coudray <amaury@monpetitgazon.com>
2021-12-04 15:56:38 +0100

    feat Position

---
+++ src/Position.test.ts
@@ -0,0 +1,51 @@
+import Position from "./Position";
+import { INDEX_MAX, INDEX_MIN } from "./utils/board";
+const X = 1;
+const Y = 2;
+const infMin = INDEX_MIN - 3;
+const supMax = INDEX_MAX + 2;
+
+const position = new Position(X, Y);
+
+const positionYInfMin = new Position(X, infMin);
+const positionYISupMin = new Position(X, supMax);
+const positionXInfMin = new Position(infMin, Y);
+const positionXISupMin = new Position(supMax, Y);
+
+describe("test isInBoard()", () => {
+  it("return true when the Position is in the board and false otherwise", () => {
+    const expectInBoardToBe = (position: Position, toBe: boolean) =>
+      expect(position.isInBoard()).toBe(toBe);
+
+    expectInBoardToBe(position, true);
+
+    expectInBoardToBe(positionYInfMin, false);
+    expectInBoardToBe(positionYISupMin, false);
+    expectInBoardToBe(positionXInfMin, false);
+    expectInBoardToBe(positionXISupMin, false);
+  });
+});
+
+describe("test toStr()", () => {
+  it("for new Position(X,Y) it return '(X,Y)'", () => {
+    expect(position.toStr()).toBe(`(${X},${Y})`);
+  });
+});
+
+describe("test equals(Position p)", () => {
+  it("return true if x1,y1===x2,y2", () => {
+    expect(position.equals(position)).toBe(true);
+    expect(position.equals(positionYInfMin)).toBe(false);
+  });
+});
+
+const testMove = (start: Position, move: Position, arrived: Position) =>
+  it(`return ${arrived.toStr()} if move was ${move.toStr()} and start ${start.toStr()}`, () => {
+    expect(start.getArrivalPosition(move).equals(arrived)).toBe(true);
+  });
+describe("test getArrivalPosition(Position move)", () => {
+  testMove(new Position(X, Y), new Position(0, 1), new Position(X, Y + 1));
+  testMove(new Position(X, Y), new Position(1, 1), new Position(X + 1, Y + 1));
+  testMove(new Position(X, Y), new Position(1, 0), new Position(X + 1, Y));
+  testMove(new Position(X, Y), new Position(0, 0), new Position(X, Y));
+});
---
+++ src/Position.ts
@@ -0,0 +1,39 @@
+import { INDEX_MAX, INDEX_MIN } from "./utils/board";
+
+class Position {
+  private x: number;
+  private y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  isInBoard() {
+    return this.isCoordinateInBoard(this.x) && this.isCoordinateInBoard(this.y);
+  }
+  private isCoordinateInBoard(n: number) {
+    return n <= INDEX_MAX && n > INDEX_MIN;
+  }
+
+  toStr() {
+    return `(${this.x},${this.y})`;
+  }
+  equals(position: Position) {
+    return this.x === position.x && this.y === position.y;
+  }
+  getArrivalPosition(move: Position): Position {
+    return new Position(this.x + move.x, this.y + move.y);
+  }
+
+  static LEFT_TOP = new Position(-1, -1);
+  static LEFT_BOTTOM = new Position(-1, 1);
+  static RIGHT_TOP = new Position(1, -1);
+  static RIGHT_BOTTOM = new Position(1, 1);
+
+  static LEFT_TOP_2 = new Position(-2, -2);
+  static LEFT_BOTTOM_2 = new Position(-2, 2);
+  static RIGHT_TOP_2 = new Position(2, -2);
+  static RIGHT_BOTTOM_2 = new Position(2, 2);
+}
+export default Position;
