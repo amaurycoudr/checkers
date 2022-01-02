8d1b32ed8f705e39d6dd4c99f5e2887552c5b8c6
Amaury Coudray <amaury@monpetitgazon.com>
2022-01-02 19:54:54 +0100

    v2.9.5

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
-import packageJson from './package.json';
+
+const packageJson = require('./package.json');
 
 export default {
   input: 'src/index.ts',
